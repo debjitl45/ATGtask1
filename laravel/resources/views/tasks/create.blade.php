8a901b090a430f3de50b53312abfd5913118782c
debjit <karmadebjit@gmail.com>
2019-09-07 15:52:49 +0530

    task1
    task1

---
+++ laravel/resources/views/tasks/create.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+@section('title','Create Task')
+@section('content')
+
+<div class="row">
+	<div class="col-sm-12">
+		<h1>Create User</h1>
+		{!! Form::open(['route'=>'task.store','method'=>'POST'])!!}
+		@component('components.taskForm')
+		@endcomponent
+		{!! Form::close()!!}
+	</div>
+</div>
+@endsection
